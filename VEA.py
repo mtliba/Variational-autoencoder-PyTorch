01530af164c4b84a579e538bd48e3bd8779fe51c
marouane <mtliba@inttic.dz>
2020-04-18 08:05:07 +0200

    commit

---
+++ VEA.py
@@ -0,0 +1,153 @@
+import torch
+import torchvision
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.utils import save_image
+import torchvision.datasets
+import os
+
+def adjust_learning_rate(optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 2 every 30 epochs"""
+    lr = lr * (0.5 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    """
+    Save the training model
+    """
+    torch.save(state, filename)
+
+def denorm(x):
+    x = 0.5 * (x + 1)
+    x = x.clamp(0, 1)
+    x = x.view(x.size(0), 1, 28, 28)
+    return x
+
+if not os.path.exists('./RESULT_img'):
+    os.mkdir('./RESULT_img')
+
+batch_size = 128
+num_epochs = 300
+learning_rate = 1e-3
+
+img_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean = [0.5, 0.5, 0.5],
+                            std = [0.5, 0.5, 0.5])
+])
+
+
+# Use standard FashionMNIST dataset
+train_set = torchvision.datasets.FashionMNIST(
+    root = './data',
+    train = True,
+    download = True,
+    transform = img_transform
+)
+
+dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+
+
+class VAE(nn.Module):
+    def __init__(self):
+        super(VAE, self).__init__()
+
+        self.fc1 = nn.Linear(784, 400)
+        self.fc11 = nn.Linear(400, 400)
+        self.fc12 = nn.Linear(400, 400)
+        self.fc21 = nn.Linear(400, 20)
+        self.fc22 = nn.Linear(400, 20)
+        self.fc3 = nn.Linear(20, 400)
+        self.fc31 = nn.Linear(400, 400)
+        self.fc32 = nn.Linear(400, 400)
+        self.fc4 = nn.Linear(400, 784)
+
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        h2 = F.relu(self.fc11(h1))
+        h3 = F.relu(self.fc11(h3))
+        return self.fc21(h3), self.fc22(h3)
+
+    def reparametrize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        if torch.cuda.is_available():
+            eps = torch.cuda.FloatTensor(std.size()).normal_()
+        else:
+            eps = torch.FloatTensor(std.size()).normal_()
+        return eps.mul(std).add_(mu)
+
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        h4 = F.relu(self.fc31(h3))
+        h5 = F.relu(self.fc32(h4))
+        return F.sigmoid(self.fc4(h5))
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparametrize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+
+model = VAE()
+if torch.cuda.is_available():
+    model.cuda()
+
+reconstruction_function = nn.MSELoss(size_average=False)
+
+
+def loss_function(recon_x, x, mu, logvar):
+    """
+    recon_x: generating images
+    x: origin images
+    mu: latent mean
+    logvar: latent log variance
+    """
+    # mse loss
+    BCE = reconstruction_function(recon_x, x)  
+    # loss = 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD_element = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
+    KLD = torch.sum(KLD_element).mul_(-0.5)
+    # KL divergence
+    return BCE + KLD
+
+
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+for epoch in range(num_epochs):
+    model.train()
+    train_loss = 0
+    adjust_learning_rate(optimizer, epoch)
+    for batch_idx, data in enumerate(dataloader):
+        img, _ = data
+        img = img.view(img.size(0), -1)
+        img = Variable(img)
+        if torch.cuda.is_available():
+            img = img.cuda()
+        optimizer.zero_grad()
+        recon_batch, mu, logvar = model(img)
+        loss = loss_function(recon_batch, img, mu, logvar)
+        loss.backward()
+        train_loss += loss.data[0]
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch,
+                batch_idx * len(img),
+                len(dataloader.dataset), 100. * batch_idx / len(dataloader),
+                loss.data[0] / len(img)))
+
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+        epoch, train_loss / len(dataloader.dataset)))
+    if epoch % 30 == 0:
+        save = to_img(recon_batch.cpu().data)
+        save_image(save, './RESULT_img/image_{}.png'.format(epoch))
+        # SAVE EACH 30 EPOCH
+        save_checkpoint({
+                'epoch': epoch + 1,
+                'state_dict': model.state_dict(),
+            }, filename=os.path.join('./', 'checkpoint_{}.tar'.format(epoch)))
+
+torch.save(model.state_dict(), './FINAL.pt')  
